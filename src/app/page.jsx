e7d96b3cb9f92f59834a4ce3d9b67b1b56285cf1
owenrud <owenrudiantoso@gmail.com>
2024-08-15 15:10:16 +0800

    small bug fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
             src="https://www.sydneyspeechclinic.com.au/ssc/wp-content/uploads/2020/04/online_therapy.jpg"
             alt="..."
           />
-          <h1 className="absolute sm:w-full md:left-[15%] top-[65%] md:top-[60%] text-color-white text-xs md:text-4xl text-center   z-10 bg-color-secondary rounded-lg w-auto px-3 py-2.5 bg-opacity-50 font-bold">
+          <h1 className="absolute md:left-[15%] top-[65%] md:top-[60%] text-color-white text-xs md:text-4xl text-center   z-10 bg-color-secondary rounded-lg w-auto px-3 py-2.5 bg-opacity-50 font-bold">
             Online Speech Therapy and Occupational Therapy Available
           </h1>
         </div>
@@ -59,11 +59,11 @@ export default function Home() {
           </h1>
         </div>
         <div className="relative">
-          <img
+          <img className="w-full"
             src="https://www.sydneyspeechclinic.com.au/ssc/wp-content/uploads/2019/02/022A5408_1000.jpg"
             alt="..."
           />
-          <h1 className="absolute md:left-[15%] top-[65%] md:top-[60%]text-color-white text-color-white text-xs md:text-2xl text-center   z-10 bg-color-secondary rounded-lg w-auto px-3 py-2.5 bg-opacity-50 font-bold">
+          <h1 className="absolute md:left-[15%] top-[65%] md:top-[55%] text-color-white text-color-white text-xs md:text-2xl text-center   z-10 bg-color-secondary rounded-lg w-auto px-3 py-2.5 bg-opacity-50 font-bold">
             Speech Therapy and Occupational Therapy Clinic
           </h1>
         </div>
@@ -114,9 +114,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row items-center justify-evenly space-y-8 space-x-4 mt-16 p-8 ">
+      <div className="flex flex-col md:flex-row  justify-evenly space-y-8 space-x-4 mt-16 p-8 ">
         <img className="shadow-lg" src="https://www.sydneyspeechclinic.com.au/ssc/wp-content/uploads/2016/02/CPSP.png"></img>
-        <iframe className="w-80 h-64 md:w-full md:h-full" style={{border: "0"}} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3314.949283667764!2d151.1684715901508!3d-33.81362185368105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xabc85055d076c3c3!2sSydney+Speech+Clinic!5e0!3m2!1sen!2sau!4v1535514902945" width="600" height="450" frameborder="0" allowfullscreen="allowfullscreen"></iframe>
+        <iframe className="w-64 h-64 md:w-full md:h-96" style={{border: "0"}} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3314.949283667764!2d151.1684715901508!3d-33.81362185368105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xabc85055d076c3c3!2sSydney+Speech+Clinic!5e0!3m2!1sen!2sau!4v1535514902945" width="600" height="450" frameborder="0" allowFullScreen="allowfullscreen"></iframe>
         </div>
         <FooterComponent/>
     </div>
